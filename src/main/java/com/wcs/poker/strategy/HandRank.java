ccfded2ddcf506d90923664f03d8579ab7cbeec9
tanosgabi <tanosgabi@gmail.com>
2015-03-24 01:48:40 +0100

    HandRank enum a lehetséges koombináciok

---
+++ src/main/java/com/wcs/poker/strategy/HandRank.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wcs.poker.strategy;
+
+/**
+ *
+ * @author Gábor
+ */
+public enum HandRank {
+    highCard,
+    pair,
+    twoPair,
+    drill,
+    straight,
+    flush,
+    fullHouse,
+    poker,
+    straightFlush,
+    royalFlush
+}
