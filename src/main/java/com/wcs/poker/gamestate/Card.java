5dd306f4952598289420a668d5d7fd212f4bf8ce
tanosgabi <tanosgabi@gmail.com>
2015-03-24 01:43:54 +0100

    2 static final array ranks and suits 2 boolean method isSuitEqual(Card ) and isSuit(String )

--- src/main/java/com/wcs/poker/gamestate/Card.java
+++ src/main/java/com/wcs/poker/gamestate/Card.java
@@ -7,10 +7,27 @@ import com.google.gson.annotations.Expose;
 @Generated("org.jsonschema2pojo")
 public class Card {
 
+    private static final String[] ranks={"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+    private static final String[] suits={"clubs","spades","hearts","diamonds"};
+    
     @Expose
     private String rank;
     @Expose
     private String suit;
+    
+    public boolean isSuitEqual(Card other){
+        if (other.getSuit().equals(this.suit)) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean isSuit(String suit) {
+        if (suit.equals(this.suit)) {
+            return true;
+        }
+        return false;
+    }
     
     public boolean isRankEqual(Card other) {
         if (other.getRank().equals(this.rank)) {
@@ -68,5 +85,14 @@ public class Card {
     public void setSuit(String suit) {
         this.suit = suit;
     }
+
+    public static String[] getSequence() {
+        return ranks;
+    }
+
+    public static String[] getSuits() {
+        return suits;
+    }
+    
 
 }
