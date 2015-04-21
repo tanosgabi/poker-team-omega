0ca102f7253b833485ac7e41364aa3241de94a69
Timi <fe.timea@gmail.com>
2015-04-21 21:19:56 +0200

    #9 straight flush elkezdve

--- src/main/java/com/wcs/poker/hand/HandRankingService.java
+++ src/main/java/com/wcs/poker/hand/HandRankingService.java
@@ -3,8 +3,11 @@ package com.wcs.poker.hand;
 import com.wcs.poker.gamestate.Card;
 import com.wcs.poker.gamestate.CardOrder;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -18,30 +21,43 @@ public class HandRankingService {
         if (cards.size() >= 8 || cards.size() <= 4) {
             throw new IllegalArgumentException("");
         }
-        
-        Hand hand = null;
 
-        if (isRoyalFlush(cards)) {
-            hand = new Hand(HandRank.ROYAL_FLUSH, handCards);
+        if (containsRoyalFlush(cards)) {
+            return new Hand(HandRank.ROYAL_FLUSH, handCards);
+        }
+        
+        if (containsStraightFlush(cards)) {
+            return new Hand(HandRank.STRAIGHT_FLUSH, handCards);
         }
 
-        return hand;
+        return null;
     }
 
     private List<Card> orderCards(List<Card> cards, CardOrder order) {
         for (Card card : cards) {
             card.setOrder(order);
         }
-        Object[] cardArray = cards.toArray();
-        Arrays.sort(cardArray);
-        cards.clear();
-        for (Object card : cardArray) {
-            cards.add((Card) card);
-        }
+        Collections.sort(cards);
         return cards;
     }
-
-    private boolean isRoyalFlush(List<Card> cards) {
+    
+    private String getRelevantSuit(List<Card> cards, int limit) {
+//        Map suitCounterMap = new HashMap();
+//        
+//        for (String suit : Card.getSuits()) {
+//            suitCounterMap.put(suit, 0);
+//        }
+//        
+//        for (Card card : cards) {
+//            for (Iterator it = suitCounterMap.keySet().iterator(); it.hasNext();) {
+//                Object key = it.next();
+//                if (suitCounterMap.get(key) == card.getSuit()) {
+//                    suitCounterMap.put(key, suitCounterMap.get(key) + 1);
+//                    break;
+//                }
+//            }
+//        }
+        
         int[] countSuits = {0, 0, 0, 0};
         String suit = null;
         
@@ -58,10 +74,17 @@ public class HandRankingService {
             }
         }
         for (int i = 0; i < countSuits.length; i++) {
-            if (countSuits[i] >= 5) {
+            if (countSuits[i] >= limit) {
                 suit = Card.getSuits()[i];
             }
         }
+        
+        return suit;
+    }
+
+    private boolean containsRoyalFlush(List<Card> cards) {
+        String suit = getRelevantSuit(cards, 5);
+        
         if (suit == null) {
             return false;
         }
@@ -81,6 +104,27 @@ public class HandRankingService {
             handCards = temp;
             return true;
         }
+        
+        return false;
+    }
+
+    private boolean containsStraightFlush(List<Card> cards) {
+        String suit = getRelevantSuit(cards, 5);
+        
+        if (suit == null) {
+            return false;
+        }
+        
+        List<Card> temp = new ArrayList<>();
+        
+        cards = orderCards(cards, CardOrder.BY_RANK);
+        for (Card card : cards) {
+            if (card.getSuit().equals(suit)) {
+                temp.add(card);
+            }
+        }
+        
+        // !!! FÉLKÉSZ !!!
         
         return false;
     }
